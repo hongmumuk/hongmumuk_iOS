0421ec6c20a8ac4687201dea72735462425afc8d
lyoodong <dwryu3079@naver.com>
2025-02-18 18:16:51 +0900

    ✨entity 추가

--- hongmumuk_SwiftUI/Enums/Sort.swift
+++ hongmumuk_SwiftUI/Enums/Sort.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
-enum Sort: String, Codable {
+enum Sort: String, Codable, CaseIterable {
     case likes
     case front
     case back
     case name
+}
+
+extension Sort {
+    var displayName: String {
+        switch self {
+        case .likes:
+            "찜 많은 순"
+        case .front:
+            "정문에서 가까운 순"
+        case .back:
+            "후문에서 가까운 순"
+        case .name:
+            "이름순"
+        }
+    }
 }
