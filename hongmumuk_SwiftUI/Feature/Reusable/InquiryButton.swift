e26e390019ac0eed788b6e9266bf0718d5d31c7c
lyoodong <dwryu3079@naver.com>
2025-02-18 19:04:43 +0900

    ♻️재사용 가능한 요소 리펙토링

--- hongmumuk_SwiftUI/Feature/Reusable/InquiryButton.swift
+++ hongmumuk_SwiftUI/Feature/Reusable/InquiryButton.swift
@@ -5,3 +5,58 @@
 //  Created by Dongwan Ryoo on 2/18/25.
 //
 
+import ComposableArchitecture
+import SwiftUI
+
+struct InquiryButton: View {
+    let action: () -> Void
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            inquryText
+            inquryBoxButton
+        }
+    }
+    
+    private var inquryText: some View {
+        Text("지금 찾고 있는 식당이 없다면?")
+            .fontStyle(Fonts.heading3Medium)
+            .foregroundColor(Colors.GrayScale.grayscal45)
+            .padding(.bottom, 12)
+    }
+    
+    private var inquryBoxButton: some View {
+        Button(action: action) {
+            inquryBox
+        }
+        .padding(.horizontal, 20)
+        .frame(height: 72)
+    }
+    
+    private var inquryBox: some View {
+        HStack(spacing: 0) {
+            Image("trumpetIcon")
+                .frame(width: 24, height: 24)
+                .padding(.trailing, 4)
+            
+            Text("식당 문의 (추가 등록, 폐점 신고)")
+                .fontStyle(Fonts.heading3Bold)
+                .foregroundColor(Colors.GrayScale.grayscale95)
+            
+            Spacer()
+            
+            Text("문의하기")
+                .fontStyle(Fonts.body2SemiBold)
+                .foregroundColor(Colors.Primary.primary55)
+        }
+        .frame(height: 72)
+        .padding(.horizontal, 20)
+        .background(backgroundView)
+    }
+    
+    private var backgroundView: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Colors.GrayScale.grayscale5)
+            .stroke(Colors.Border.normal)
+    }
+}
