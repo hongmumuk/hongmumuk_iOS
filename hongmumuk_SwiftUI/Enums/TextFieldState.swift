796cf64a981fe07105cd69bed240ad5285bfdda5
30 <1224tpdud@gmail.com>
2025-02-21 21:32:18 +0900

    ✨ TextFieldStatus 추가

--- hongmumuk_SwiftUI/Enums/TextFieldState.swift
+++ hongmumuk_SwiftUI/Enums/TextFieldState.swift
@@ -6,11 +6,59 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum TextFiledState: Equatable {
-    case none
-    case valid
-    case validGreen(String)
-    case invalid(String)
+enum TextFieldStatus {
+    case `default`
     case focused
+    case invalid
+    case valid
+}
+
+struct TextFieldState: Equatable {
+    var text: String = ""
+    var status: TextFieldStatus = .default
+    var errorMessage: String? = nil
+
+    mutating func updateStatus(isFocused: Bool, isValid: Bool, message: String? = nil) {
+        if isFocused {
+            if status == .invalid {
+                return
+            } else {
+                status = .focused
+            }
+        } else {
+            status = isValid ? .valid : .invalid
+            errorMessage = isValid ? nil : message
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch status {
+        case .default, .focused, .valid:
+            return Color(hex: "FBFBFE")
+        case .invalid:
+            return Color(hex: "FFE8E5")
+        }
+    }
+    
+    var borderColor: Color {
+        switch status {
+        case .default, .valid:
+            return Colors.Border.strong
+        case .focused:
+            return Colors.Primary.normal
+        case .invalid:
+            return Colors.SemanticColor.negative
+        }
+    }
+    
+    var textColor: Color {
+        switch status {
+        case .default, .focused, .valid:
+            return Colors.GrayScale.normal
+        case .invalid:
+            return Colors.SemanticColor.negative
+        }
+    }
 }
