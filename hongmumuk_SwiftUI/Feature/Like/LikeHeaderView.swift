4582bdb9f9cc0c92cadde706fec599865c255f19
lyoodong <dwryu3079@naver.com>
2025-02-22 15:28:12 +0900

    💄like UI 추가

--- hongmumuk_SwiftUI/Feature/Like/LikeHeaderView.swift
+++ hongmumuk_SwiftUI/Feature/Like/LikeHeaderView.swift
@@ -4,3 +4,45 @@
 //
 //  Created by Dongwan Ryoo on 2/18/25.
 //
+
+import ComposableArchitecture
+import SwiftUI
+
+struct LikeHeaderView: View {
+    @ObservedObject var viewStore: ViewStoreOf<LikeFeature>
+    
+    var body: some View {
+        VStack {
+            header
+                .frame(height: 56)
+                .padding(.horizontal, 24)
+                .overlay(alignment: .bottom, content: {
+                    Group {
+                        if viewStore.sortedRestaurantList.isEmpty {
+                            bottomBorderLine
+                        }
+                    }
+                })
+        }
+    }
+    
+    private var bottomBorderLine: some View {
+        Rectangle()
+            .frame(height: 1)
+            .foregroundColor(Colors.Border.neutral)
+    }
+    
+    private var header: some View {
+        HStack(spacing: 0) {
+            likeText
+            Spacer()
+        }
+        .padding(.horizontal, 24)
+    }
+    
+    private var likeText: some View {
+        Text("찜")
+            .fontStyle(Fonts.title2Bold)
+            .foregroundColor(Colors.GrayScale.grayscale95)
+    }
+}
--- hongmumuk_SwiftUI/Feature/Like/LikeListView.swift
+++ hongmumuk_SwiftUI/Feature/Like/LikeListView.swift
@@ -4,3 +4,47 @@
 //
 //  Created by Dongwan Ryoo on 2/18/25.
 //
+
+import ComposableArchitecture
+import Shimmer
+import SwiftUI
+
+struct LikeListView: View {
+    @ObservedObject var viewStore: ViewStoreOf<LikeFeature>
+    
+    var body: some View {
+        if viewStore.showSkeletonLoading {
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(0 ..< 5, id: \.self) { _ in
+                        ListItemSkeletonItemView()
+                            .shimmering(active: true)
+                    }
+                }
+            }
+            .padding(.top, 16)
+        } else {
+            if viewStore.sortedRestaurantList.isEmpty {
+                VStack {
+                    Spacer()
+                    LikeEmptyView()
+                    Spacer()
+                }
+            } else {
+                VStack(spacing: 0) {
+                    LikeFilterView(viewStore: viewStore)
+                    ScrollView {
+                        LazyVStack(spacing: 0) {
+                            ForEach(viewStore.sortedRestaurantList) { item in
+                                ListItemView(item: item, sort: viewStore.sort) {
+                                    viewStore.send(.restrauntTapped(id: $0.id))
+                                }
+                            }
+                        }
+                    }
+                    .padding(.top, 16)
+                }
+            }
+        }
+    }
+}
