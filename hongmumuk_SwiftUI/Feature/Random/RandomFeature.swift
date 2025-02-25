2b5dad7d543ac22b14dbdb573ea85fb4df7e1af4
lyoodong <dwryu3079@naver.com>
2025-02-25 12:55:49 +0900

    🚚 랜덤 기능에 필요한 리소스 추가

---
+++ hongmumuk_SwiftUI/Feature/Random/RandomFeature.swift
@@ -0,0 +1,8 @@
+//
+//  RandomFeature.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 2/25/25.
+//
+
+import Foundation
---
+++ hongmumuk_SwiftUI/Feature/Random/RandomView.swift
@@ -0,0 +1,8 @@
+//
+//  RandomView.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 2/25/25.
+//
+
+import Foundation
