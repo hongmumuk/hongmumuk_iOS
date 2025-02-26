e8f82b9b19ad872ccfb8004d62dd55663f4eff76
30 <1224tpdud@gmail.com>
2025-02-26 21:14:53 +0900

    ✨ 이메일, 패스워드 글자 수 길이 제한 추가

--- hongmumuk_SwiftUI/Dependency/Client/ValidationClient.swift
+++ hongmumuk_SwiftUI/Dependency/Client/ValidationClient.swift
@@ -16,10 +16,14 @@ extension ValidationClient: DependencyKey {
     static func live(univType: Univ) -> Self {
         return .init(
             validateEmail: { email in
-                email.range(of: univType.emailRegex, options: .regularExpression) != nil
+                let isValidLength = (3 ... 20).contains(email.count)
+                let matchesRegex = email.range(of: univType.emailRegex, options: .regularExpression) != nil
+                return isValidLength && matchesRegex
             },
             validatePassword: { password in
-                password.range(of: univType.passwordRegex, options: .regularExpression) != nil
+                let isValidLength = (8 ... 20).contains(password.count)
+                let matchesRegex = password.range(of: univType.passwordRegex, options: .regularExpression) != nil
+                return isValidLength && matchesRegex
             }
         )
     }
