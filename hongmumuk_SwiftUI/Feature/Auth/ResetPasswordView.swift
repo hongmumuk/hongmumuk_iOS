9635346a5b35a96c008317c6aa7a7bde6e1966aa
30 <1224tpdud@gmail.com>
2025-03-02 18:44:53 +0900

    ✨ FindPassword View 추가

--- hongmumuk_SwiftUI/Feature/Auth/ResetPasswordView.swift
+++ hongmumuk_SwiftUI/Feature/Auth/ResetPasswordView.swift
@@ -5,4 +5,114 @@
 //  Created by Park Seyoung on 3/1/25.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct ResetPasswordView: View {
+    let store: StoreOf<ResetPasswordFeature>
+
+    @ObservedObject var viewStore: ViewStoreOf<ResetPasswordFeature>
+    @FocusState private var isPasswordFocused: Bool
+    @FocusState private var isVerifiedPasswordFocused: Bool
+
+    init(store: StoreOf<ResetPasswordFeature>) {
+        self.store = store
+        viewStore = ViewStore(store, observe: { $0 })
+    }
+
+    var body: some View {
+        NavigationStack {
+            GeometryReader { geometry in
+                VStack(alignment: .leading) {
+                    Divider()
+                        .background(Colors.Border.neutral)
+                        .frame(height: 1)
+                    
+                    Text("비밀번호")
+                        .fontStyle(Fonts.heading2Bold)
+                        .foregroundStyle(CommonTextFieldStyle.textColor(for: viewStore.passwordState))
+                        .padding(.leading, 24)
+                        .padding(.top, geometry.size.height * 0.04)
+                    
+                    CommonTextFieldView(
+                        isFocused: $isPasswordFocused,
+                        text: viewStore.password,
+                        state: viewStore.passwordState,
+                        message: viewStore.passwordErrorMessage,
+                        placeholder: "영문, 숫자 포함 8~20자 이내로 입력해 주세요",
+                        isSecure: true,
+                        showAtSymbol: false,
+                        showSuffix: false,
+                        suffixText: "",
+                        onTextChanged: { viewStore.send(.passwordChanged($0)) },
+                        onFocusedChanged: { viewStore.send(.passwordFocused($0)) },
+                        onSubmit: { viewStore.send(.passwordOnSubmit) },
+                        onClear: { viewStore.send(.passwordTextClear) }
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.top, 8)
+                    
+                    Text("비밀번호 확인")
+                        .fontStyle(Fonts.heading2Bold)
+                        .foregroundStyle(CommonTextFieldStyle.textColor(for: viewStore.verifiedPasswordState))
+                        .padding(.leading, 24)
+                        .padding(.top, 24)
+                    
+                    CommonTextFieldView(
+                        isFocused: $isVerifiedPasswordFocused,
+                        text: viewStore.verifiedPassword,
+                        state: viewStore.verifiedPasswordState,
+                        message: viewStore.verifiedPasswordMessage,
+                        placeholder: "비밀번호를 한번 더 입력해 주세요",
+                        isSecure: true,
+                        showAtSymbol: false,
+                        showSuffix: false,
+                        suffixText: "",
+                        onTextChanged: { viewStore.send(.verifiedPasswordChanged($0)) },
+                        onFocusedChanged: { viewStore.send(.verifiedPasswordFocused($0)) },
+                        onSubmit: { viewStore.send(.verifiedPasswordOnSubmit) },
+                        onClear: { viewStore.send(.verifiedPasswordTextClear) }
+                    )
+                    
+                    .padding(.horizontal, 24)
+                    .padding(.top, 8)
+                    
+                    Spacer()
+                    
+                    NextButton(title: "비밀번호 재설정하기", isActive: viewStore.isResetPasswordButtonEnabled) {
+                        if viewStore.isResetPasswordButtonEnabled {
+                            viewStore.send(.resetPasswordButtonTapped)
+                        }
+                    }
+                    .frame(height: 60)
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, geometry.size.height * 0.1)
+                }
+            }
+            .navigationTitle("비밀번호 재설정")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("비밀번호 재설정")
+                        .fontStyle(Fonts.heading1Bold)
+                        .foregroundColor(Colors.GrayScale.normal)
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { viewStore.send(.backButtonTapped) }) {
+                        Image("backButton")
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                    }
+                    .padding(.leading, 4)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                isPasswordFocused = false
+                isVerifiedPasswordFocused = false
+            }
+        }
+    }
+}
--- hongmumuk_SwiftUI/Feature/Auth/VerifyEmailView.swift
+++ hongmumuk_SwiftUI/Feature/Auth/VerifyEmailView.swift
@@ -5,14 +5,133 @@
 //  Created by Park Seyoung on 3/1/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 struct VerifyEmailView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    let store: StoreOf<VerifyEmailFeature>
+    
+    @ObservedObject var viewStore: ViewStoreOf<VerifyEmailFeature>
+    @FocusState private var isEmailFocused: Bool
+    @FocusState private var isCodeFocused: Bool
+    
+    init(store: StoreOf<VerifyEmailFeature>) {
+        self.store = store
+        viewStore = ViewStore(store, observe: { $0 })
     }
-}
-
-#Preview {
-    VerifyEmailView()
+    
+    var body: some View {
+        NavigationStack {
+            GeometryReader { geometry in
+                VStack(alignment: .leading) {
+                    Divider()
+                        .background(Colors.Border.neutral)
+                        .frame(height: 1)
+                    
+                    Text("이메일")
+                        .fontStyle(Fonts.heading2Bold)
+                        .foregroundStyle(CommonTextFieldStyle.textColor(for: viewStore.emailState))
+                        .padding(.leading, 24)
+                        .padding(.top, geometry.size.height * 0.04)
+                    
+                    CommonTextFieldView(
+                        isFocused: $isEmailFocused,
+                        text: viewStore.email,
+                        state: viewStore.emailState,
+                        message: viewStore.emailErrorMessage,
+                        placeholder: "학교 이메일을 입력해 주세요",
+                        isSecure: false,
+                        showAtSymbol: true,
+                        showSuffix: true,
+                        suffixText: "g.hongik.ac.kr",
+                        onTextChanged: { viewStore.send(.emailChanged($0)) },
+                        onFocusedChanged: { viewStore.send(.emailFocused($0)) },
+                        onSubmit: { viewStore.send(.emailOnSubmit) },
+                        onClear: { viewStore.send(.emailTextClear) }
+                    )
+                    .padding(.horizontal, 24)
+                    .padding(.top, 8)
+                    
+                    BasicButton(title: "인증번호 발송", isActive: viewStore.isSendCodeButtonEnabled) {
+                        if viewStore.isSendCodeButtonEnabled {
+                            viewStore.send(.sendCodeButtonTapped)
+                        }
+                    }
+                    .frame(height: 48)
+                    .padding(.horizontal, 24)
+                    .padding(.top, 8)
+                    
+                    Text("인증번호")
+                        .fontStyle(Fonts.heading2Bold)
+                        .foregroundStyle(CommonTextFieldStyle.textColor(for: viewStore.codeState))
+                        .padding(.leading, 24)
+                        .padding(.top, 24)
+                    
+                    HStack(alignment: .top) {
+                        CommonTextFieldView(
+                            isFocused: $isCodeFocused,
+                            text: viewStore.code,
+                            state: viewStore.codeState,
+                            message: viewStore.codeErrorMessage,
+                            placeholder: "6자리 숫자를 입력해 주세요",
+                            isSecure: false,
+                            showAtSymbol: false,
+                            showSuffix: false,
+                            suffixText: "",
+                            onTextChanged: { viewStore.send(.codeChanged($0)) },
+                            onFocusedChanged: { viewStore.send(.codeFocused($0)) },
+                            onSubmit: { viewStore.send(.codeOnSubmit) },
+                            onClear: { viewStore.send(.codeTextClear) }
+                        )
+                        .frame(width: geometry.size.width * 0.62)
+                        
+                        BasicButton(title: "인증하기", isActive: viewStore.isVerifyCodeButtonEnabled) {
+                            if viewStore.isVerifyCodeButtonEnabled {
+                                viewStore.send(.verifyCodeButtonTapped)
+                            }
+                        }
+                        .padding(.leading, 8)
+                        .frame(height: 48)
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.top, 8)
+                    
+                    Spacer()
+                    
+                    NextButton(title: "비밀번호 재설정하러 가기", isActive: viewStore.isContinueButtonEnabled) {
+                        if viewStore.isContinueButtonEnabled {
+                            viewStore.send(.continueButtonTapped)
+                        }
+                    }
+                    .frame(height: 60)
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, geometry.size.height * 0.1)
+                }
+            }
+            .navigationTitle("비밀번호 찾기")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("비밀번호 찾기")
+                        .fontStyle(Fonts.heading1Bold)
+                        .foregroundColor(Colors.GrayScale.normal)
+                }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: { viewStore.send(.backButtonTapped) }) {
+                        Image("backButton")
+                            .resizable()
+                            .frame(width: 36, height: 36)
+                    }
+                    .padding(.leading, 4)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .contentShape(Rectangle())
+            .onTapGesture {
+                isEmailFocused = false
+                isCodeFocused = false
+            }
+        }
+    }
 }
