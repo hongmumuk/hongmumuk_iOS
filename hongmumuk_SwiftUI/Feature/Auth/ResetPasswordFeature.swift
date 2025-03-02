20a3f1108bb742c300a89ecc2187c18442900a77
30 <1224tpdud@gmail.com>
2025-03-02 18:07:43 +0900

    ✨ FindPassword Feature 추가

--- hongmumuk_SwiftUI/Feature/Auth/ResetPasswordFeature.swift
+++ hongmumuk_SwiftUI/Feature/Auth/ResetPasswordFeature.swift
@@ -5,4 +5,130 @@
 //  Created by Park Seyoung on 3/1/25.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct ResetPasswordFeature: Reducer {
+    // ActiveScreen은 EmailLoginFeature에서 관리
+    struct State: Equatable {
+        var password: String = ""
+        var passwordErrorMessage: String? = nil
+        var passwordState: TextFieldState = .empty
+        
+        var verifiedPassword: String = ""
+        var verifiedPasswordErrorMessage: String? = nil
+        var verifiedPasswordState: TextFieldState = .empty
+        
+        var resetPasswordError: LoginError? = nil
+        
+        var isResetPasswordLoading: Bool = false
+        
+        var isResetPasswordButtonEnabled: Bool {
+            // 여기서 밸리드는 같냐 안같냐로 결정
+            passwordState == .valid && verifiedPasswordState == .valid
+        }
+    }
+    
+    enum Action: Equatable {
+        case passwordChanged(String)
+        case verifiedPasswordChanged(String)
+        
+        case passwordFocused(Bool)
+        case verifiedPasswordFocused(Bool)
+        
+        case passwordOnSubmit
+        case verifiedPasswordOnSubmit
+        
+        case passwordTextClear
+        case verifiedPasswordTextClear
+        
+        case resetPasswordButtonTapped
+        case backButtonTapped
+        
+        case successReset, failReset(LoginError)
+        case onDismiss
+    }
+    
+    @Dependency(\.validationClient) var validationClient
+    @Dependency(\.authClient) var authClient
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .passwordChanged(password):
+                state.password = password
+                return .none
+                
+            case let .verifiedPasswordChanged(verifiedPassword):
+                state.verifiedPassword = verifiedPassword
+                return .none
+                
+            case let .passwordFocused(isFocused):
+                state.passwordState = isFocused ? .focused : (state.password.isEmpty ? .empty : .normal)
+                state.passwordErrorMessage = nil
+                return .none
+                
+            case let .verifiedPasswordFocused(isFocueed):
+                state.verifiedPasswordState = isFocueed ? .focused : (state.verifiedPassword.isEmpty ? .empty : .normal)
+                state.verifiedPasswordErrorMessage = nil
+                return .none
+                
+            case .passwordOnSubmit:
+                if state.password.isEmpty {
+                    state.passwordState = .empty
+                    state.passwordErrorMessage = nil
+                } else if !validationClient.validatePassword(state.password) {
+                    state.passwordState = .invalid
+                    state.passwordErrorMessage = "이메일 형식이 잘못되었습니다."
+                } else {
+                    state.passwordState = .valid
+                    state.passwordErrorMessage = nil
+                }
+                return .none
+            
+            case .verifiedPasswordOnSubmit:
+                if state.verifiedPassword.isEmpty {
+                    state.verifiedPasswordState = .empty
+                    state.verifiedPasswordErrorMessage = nil
+                } else if state.verifiedPassword == state.password {
+                    state.passwordState = .valid
+                    state.passwordErrorMessage = nil
+                } else {
+                    state.passwordState = .invalid
+                    state.passwordErrorMessage = "비밀번호가 일치하지 않습니다."
+                }
+                return .none
+                
+            case .passwordTextClear:
+                state.password = ""
+                state.passwordState = .empty
+                state.passwordErrorMessage = nil
+                return .none
+                
+            case .verifiedPasswordTextClear:
+                state.verifiedPassword = ""
+                state.verifiedPasswordState = .empty
+                state.verifiedPasswordErrorMessage = nil
+                return .none
+                
+            case .resetPasswordButtonTapped:
+                state.isResetPasswordLoading = true
+                return .none
+                
+            case .backButtonTapped, .onDismiss:
+                return .none
+                
+            case .successReset:
+                state.isResetPasswordLoading = false
+                // 다음 화면
+                
+                return .none
+                
+            case let .failReset(error):
+                state.isResetPasswordLoading = false
+                
+                return .none
+            }
+        }
+    }
+}
--- hongmumuk_SwiftUI/Feature/Auth/VerifyEmailFeature.swift
+++ hongmumuk_SwiftUI/Feature/Auth/VerifyEmailFeature.swift
@@ -5,4 +5,202 @@
 //  Created by Park Seyoung on 3/1/25.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct VerifyEmailFeature: Reducer {
+    // ActiveScreen은 EmailLoginFeature에서 관리
+    struct State: Equatable {
+        var email: String = ""
+        var emailErrorMessage: String? = nil
+        var emailState: TextFieldState = .empty
+        
+        var code: String = ""
+        var codeErrorMessage: String? = nil
+        var codeState: TextFieldState = .empty
+        
+        // loginError에 따라 인증 됐는지 여부 파악
+        var sendCodeError: LoginError? = nil
+        var verifyCodeError: LoginError? = nil
+        
+        var isSendCodeLoading: Bool = false
+        var isVerifyCodeLoading: Bool = false
+        var isContinueLoading: Bool = false
+        
+        var isSendCodeButtonEnabled: Bool {
+            emailState == .valid
+        }
+
+        var isVerifyCodeButtonEnabled: Bool {
+            emailState == .valid && codeState == .valid
+        }
+
+        var isContinueButtonEnabled: Bool = false
+    }
+    
+    enum Action: Equatable {
+        case emailChanged(String)
+        case codeChanged(String)
+        
+        case emailFocused(Bool)
+        case codeFocused(Bool)
+        
+        case emailOnSubmit
+        case codeOnSubmit
+        
+        case emailTextClear
+        case codeTextClear
+        
+        case sendCodeButtonTapped
+        case verifyCodeButtonTapped
+        case continueButtonTapped
+        case backButtonTapped
+        
+        case successSend, failSend(LoginError)
+        case successVerify, failVerify(LoginError)
+        case onDismiss
+    }
+    
+    @Dependency(\.validationClient) var validationClient
+    @Dependency(\.authClient) var authClient
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .emailChanged(email):
+                state.email = email
+                return .none
+                
+            case let .codeChanged(code):
+                state.code = code
+                return .none
+                
+            case let .emailFocused(isFocused):
+                state.emailState = isFocused ? .focused : (state.email.isEmpty ? .empty : .normal)
+                state.emailErrorMessage = nil
+                if state.sendCodeError != nil {
+                    state.sendCodeError = nil
+                }
+                return .none
+                
+            case let .codeFocused(isFocueed):
+                state.codeState = isFocueed ? .focused : (state.code.isEmpty ? .empty : .normal)
+                state.codeErrorMessage = nil
+                if state.verifyCodeError != nil {
+                    state.verifyCodeError = nil
+                }
+                return .none
+                
+            case .emailOnSubmit:
+                if state.email.isEmpty {
+                    state.emailState = .empty
+                    state.emailErrorMessage = nil
+                } else if !validationClient.validateEmail(state.email) {
+                    state.emailState = .invalid
+                    state.emailErrorMessage = "이메일 형식이 잘못되었습니다."
+                } else {
+                    state.emailState = .valid
+                    state.emailErrorMessage = nil
+                }
+                return .none
+            
+            case .codeOnSubmit:
+                if state.code.isEmpty {
+                    state.codeState = .empty
+                    state.codeErrorMessage = nil
+                } else if state.code.count != 6 {
+                    state.codeState = .invalid
+                    state.codeErrorMessage = "6자리 숫자를 입력해주세요"
+                } else {
+                    state.codeState = .valid
+                    state.codeErrorMessage = nil
+                }
+                return .none
+                
+            case .emailTextClear:
+                state.email = ""
+                state.emailState = .empty
+                state.emailErrorMessage = nil
+                return .none
+                
+            case .codeTextClear:
+                state.code = ""
+                state.codeState = .empty
+                state.codeErrorMessage = nil
+                return .none
+                
+            case .sendCodeButtonTapped:
+                state.isSendCodeLoading = true
+                let newEmail = "\(state.email)@g.hongik.ac.kr"
+                let body = SendVerifyCodeModel(email: newEmail, join: false)
+                return .run { send in
+                    do {
+                        try await authClient.sendVerificationEmail(body)
+                        await send(.successSend)
+                    } catch {
+                        if let sendError = error as? LoginError {
+                            await send(.failSend(sendError))
+                        }
+                    }
+                }
+                
+            case .verifyCodeButtonTapped:
+                state.isVerifyCodeLoading = true
+                let newEmail = "\(state.email)@g.hongik.ac.kr"
+                let body = VerifyEmailModel(email: newEmail, code: state.code)
+                return .run { send in
+                    do {
+                        if try await authClient.verifyEmailCode(body) {
+                            await send(.successVerify)
+                        }
+                    } catch {
+                        print(error)
+                        
+                        if let loginError = error as?
+                            LoginError
+                        {
+                            await send(.failVerify(loginError))
+                        }
+                    }
+                }
+                
+            case .continueButtonTapped:
+                // 다음 화면으로 넘어가야하는데 이걸 부모뷰에서 조정할지...
+                return .none
+                
+            case .backButtonTapped, .onDismiss:
+                return .none
+                
+            case .successSend:
+                state.isSendCodeLoading = false
+                // 보내고 UI 업데이트
+                return .none
+                
+            case let .failSend(error):
+                state.isSendCodeLoading = false
+                state.sendCodeError = error
+                if state.sendCodeError != nil {
+                    state.emailState = .loginError
+                }
+                state.emailErrorMessage = error == .userNotFound ? "가입된 계정이 없습니다. 이메일을 다시 확인해주세요." : nil
+                return .none
+                
+            case .successVerify:
+                state.isVerifyCodeLoading = false
+                state.isContinueButtonEnabled = true
+                state.emailState = .codeVerified
+                state.codeState = .disabled
+                
+                return .none
+                
+            case let .failVerify(error):
+                state.isVerifyCodeLoading = false
+                state.codeState = .codeInvalid
+                state.codeErrorMessage = error == .invalidCode ? "인증번호가 잘못 입력되었습니다." : nil
+                state.codeErrorMessage = error == .noVerificationRecord ? "인증번호가 전송되지 않았습니다." : nil
+                
+                return .none
+            }
+        }
+    }
+}
