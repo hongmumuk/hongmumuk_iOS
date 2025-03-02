de20371eaed3ff483ab55be28aaf4d001f73cb4f
30 <1224tpdud@gmail.com>
2025-03-02 18:57:29 +0900

    🚚 Signup Resource 추가

---
+++ hongmumuk_SwiftUI/Feature/Auth/SignupFeature.swift
@@ -0,0 +1,8 @@
+//
+//  SignupFeature.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Park Seyoung on 3/2/25.
+//
+
+import Foundation
---
+++ hongmumuk_SwiftUI/Feature/Auth/SignupPolicyModalView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupPolicyModalView.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Park Seyoung on 3/2/25.
+//
+
+import SwiftUI
+
+struct SignupPolicyModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupPolicyModalView()
+}
