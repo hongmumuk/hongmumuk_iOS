e228fa733132e795b896f206630323af0eaf8f4f
lyoodong <dwryu3079@naver.com>
2025-03-03 14:54:32 +0900

    ♻️ Empty View 추상화를 위한 파일 추가

---
+++ hongmumuk_SwiftUI/Enums/Epmty.swift
@@ -0,0 +1,8 @@
+//
+//  Epmty.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 3/3/25.
+//
+
+import Foundation
---
+++ hongmumuk_SwiftUI/Feature/Search/SearchEmptyView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchEmptyView.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 3/3/25.
+//
+
+import Foundation
