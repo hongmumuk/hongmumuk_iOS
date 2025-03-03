20f287dda37840eab8c1cd2ba6149c56f8e906fb
DongWan <115209527+lyoodong@users.noreply.github.com>
2025-03-03 23:21:47 +0900

    [feat] Like UI 추가 (#43)
    * 💄like UI 추가
    
    * 💄화면 연결
    
    * ♻️오타 수정

--- hongmumuk_SwiftUI/Feature/Like/LikeEmptyView.swift
+++ hongmumuk_SwiftUI/Feature/Like/LikeEmptyView.swift
@@ -4,3 +4,31 @@
 //
 //  Created by Dongwan Ryoo on 2/18/25.
 //
+
+import ComposableArchitecture
+import SwiftUI
+
+struct LikeEmptyView: View {
+    var body: some View {
+        emptyView
+    }
+    
+    var emptyView: some View {
+        VStack(spacing: 0) {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Colors.GrayScale.grayscale5)
+                .stroke(Colors.GrayScale.grayscale5)
+                .frame(width: 100, height: 100)
+                .padding(.bottom, 60)
+            
+            Text("아직 찜한 가게가 없습니다")
+                .fontStyle(Fonts.title2Bold)
+                .foregroundColor(Colors.GrayScale.grayscale95)
+                .padding(.bottom, 9)
+            
+            Text("좋아하는 가게에 찜을 누르고 한 번에 모아 보세요")
+                .fontStyle(Fonts.heading3Medium)
+                .foregroundColor(Colors.GrayScale.grayscal45)
+        }
+    }
+}
--- hongmumuk_SwiftUI/Feature/Like/LikeView.swift
+++ hongmumuk_SwiftUI/Feature/Like/LikeView.swift
@@ -5,10 +5,38 @@
 //  Created by Dongwan Ryoo on 2/18/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 struct LikeView: View {
+    private let store: StoreOf<LikeFeature>
+    @ObservedObject var viewStore: ViewStoreOf<LikeFeature>
+    
+    init(store: StoreOf<LikeFeature>) {
+        self.store = store
+        viewStore = ViewStore(store, observe: { $0 })
+    }
+    
     var body: some View {
-        Text("좋아요 화면")
+        NavigationStack {
+            VStack(spacing: 0) {
+                LikeHeaderView(viewStore: viewStore)
+                LikeListView(viewStore: viewStore)
+            }
+        }
+        .onAppear {
+            viewStore.send(.onAppear)
+        }
+        .navigationDestination(
+            isPresented: viewStore.binding(
+                get: { $0.activeScreen != .none },
+                send: .onDismiss
+            )
+        ) {
+            let screen = viewStore.activeScreen
+            if case let .restaurantDetail(id) = screen {
+                // 상세화면 전환
+            }
+        }
     }
 }
