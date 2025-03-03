b5d29852b50af537e554e54496073c4f47293cc2
30 <127213160+30isdead@users.noreply.github.com>
2025-03-03 23:53:48 +0900

    [Feat] FindPassword View 추가 (#57)
    * 🎨 UI 컴포넌트 추가 및 TextField state 추가
    
    * ✨ FindPassword View 추가

--- hongmumuk_SwiftUI/Enums/TextFieldState.swift
+++ hongmumuk_SwiftUI/Enums/TextFieldState.swift
@@ -16,5 +16,5 @@ public enum TextFieldState {
     case loginError // 로그인 에러 발생
     case codeInvalid // 인증 코드가 잘못된 경우
     case codeVerified // 인증이 완료된 경우
-    case disabled //인증코드 입력 후 인증이 완료된 코드 텍스트 필드
+    case disabled // 인증코드 입력 후 인증이 완료된 코드 텍스트 필드
 }
