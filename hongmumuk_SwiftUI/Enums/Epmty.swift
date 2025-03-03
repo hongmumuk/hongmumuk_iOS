9ecdd81da69ccb9db305cb669c642b302e1dc5b6
lyoodong <dwryu3079@naver.com>
2025-03-03 14:54:53 +0900

    ✨ empty 타입 정의 및 타이틀, 서브타이틀 정의

--- hongmumuk_SwiftUI/Enums/Epmty.swift
+++ hongmumuk_SwiftUI/Enums/Epmty.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum Empty {
+    case search
+    case like
+    case likeUnAuth
+}
+
+extension Empty {
+    var title: String {
+        switch self {
+        case .search:
+            "검색 결과가 없습니다"
+        case .like:
+            "아직 찜한 가게가 없습니다"
+        case .likeUnAuth:
+            "로그인이 필요한 기능입니다"
+        }
+    }
+    
+    var subTitle: String {
+        switch self {
+        case .search:
+            "다른 단어로 검색해 보세요"
+        case .like:
+            "좋아하는 가게에 찜을 누르고 한 번에 모아 보세요"
+        case .likeUnAuth:
+            "로그인 후 찜한 가게를 모아 보세요"
+        }
+    }
+}
