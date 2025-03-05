00daea50e7fa9b7393ab27a2026eeeb04d5aeb36
30 <1224tpdud@gmail.com>
2025-03-05 11:30:30 +0900

    ✨ RootView, RootStore 추가

---
+++ hongmumuk_SwiftUI/Feature/Auth/RootFeature.swift
@@ -0,0 +1,194 @@
+//
+//  RootFeature.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Park Seyoung on 3/4/25.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct RootFeature: Reducer {
+    enum ActiveScreen: Equatable, Hashable {
+        case splash, login, emailLogin,
+             signup, signupEmail, signupPassword, signupDone,
+             verifyEmail, resetPassword,
+             home, random, search, categoryList(
+                 Category
+             )
+    }
+    
+    struct State: Equatable {
+        var navigationPath: [ActiveScreen] = []
+        var emailLogin = EmailLoginFeature.State()
+        var signupPassword = SignupPasswordFeature.State()
+        var resetPassword = ResetPasswordFeature.State()
+        
+        var isLoggedIn: Bool = false
+        var isLoading: Bool = true
+    }
+    
+    enum Action: Equatable {
+        case navigationTo(ActiveScreen)
+        case setNavigationRoot(ActiveScreen)
+        case onDismiss
+        case setNavigationPath([ActiveScreen])
+        case emailLogin(EmailLoginFeature.Action)
+        case signupPassword(SignupPasswordFeature.Action)
+        case resetPassword(ResetPasswordFeature.Action)
+        case resetAllFeatureStates
+        
+        case checkLoginStatus
+        case setLoginStatus(Bool)
+        case setLoadingStatus(Bool)
+    }
+    
+    @Dependency(\.keychainClient) var keychainClient
+    @Dependency(\.authClient) var authClient
+    
+    var body: some ReducerOf<Self> {
+        Scope(state: \State.emailLogin, action: /Action.emailLogin) {
+            EmailLoginFeature()
+        }
+        Scope(state: \State.signupPassword, action: /Action.signupPassword) {
+            SignupPasswordFeature()
+        }
+        Scope(state: \State.resetPassword, action: /Action.resetPassword) {
+            ResetPasswordFeature()
+        }
+        Reduce { state, action in
+            switch action {
+            case let .navigationTo(screen):
+                state.navigationPath.append(screen)
+                return .none
+            
+            case let .setNavigationPath(path):
+                state.navigationPath = path
+                return .none
+                
+            case let .setNavigationRoot(path):
+                state.navigationPath = [path]
+                return .none
+                
+            case .onDismiss:
+                if state.navigationPath.count > 1 {
+                    state.navigationPath.removeLast()
+                }
+                return .none
+                
+            // emailLoginFeature
+            case .emailLogin(.successLogin):
+                return .concatenate(
+                    .send(.resetAllFeatureStates),
+                    .send(.setNavigationRoot(.home))
+                )
+                
+            // signupPasswordFeature
+            case .signupPassword(.successJoin):
+                return .concatenate(
+                    .send(.resetAllFeatureStates),
+                    .send(.navigationTo(.signupDone))
+                )
+                
+            // resetPasswordFeature
+            case .resetPassword(.successReset):
+                return .concatenate(
+                    .send(.resetAllFeatureStates),
+                    .send(.setNavigationRoot(.login))
+                )
+                
+            case .resetAllFeatureStates:
+                state.emailLogin = EmailLoginFeature.State()
+                state.signupPassword = SignupPasswordFeature.State()
+                state.resetPassword = ResetPasswordFeature.State()
+                return .none
+                
+            case .checkLoginStatus:
+                return .run { send in
+                    let accessToken = await keychainClient.getString(.accessToken)
+                    let refreshToken = await keychainClient.getString(.refreshToken)
+                    print("accessToken: \(accessToken), refreshToken: \(refreshToken)")
+                    
+                    if let accessToken, !accessToken.isEmpty {
+                        // 액세스 토큰이 유효한지 확인
+                        if isValidAccessToken(accessToken) {
+                            await send(.setLoginStatus(true))
+                            await send(.setNavigationRoot(.home))
+                        } else {
+                            // 액세스 토큰 만료
+                            if let refreshToken, !refreshToken.isEmpty {
+                                do {
+                                    // 토큰 재발급 및 키체인 저장
+                                    let newTokens: AuthTokenResponseModel = try await authClient.token(accessToken, refreshToken)
+                                    await keychainClient.setString(newTokens.accessToken, .accessToken)
+                                    await keychainClient.setString(newTokens.refreshToken, .refreshToken)
+                                    
+                                    await send(.setLoginStatus(true))
+                                    await send(.setNavigationRoot(.home))
+                                } catch {
+                                    await send(.setLoginStatus(false))
+                                    await send(.setNavigationRoot(.login))
+                                }
+                            } else {
+                                await send(.setLoginStatus(false))
+                                await send(.setNavigationRoot(.login))
+                            }
+                        }
+                    } else {
+                        await send(.setLoginStatus(false))
+                    }
+                    await send(.setLoadingStatus(false))
+                }
+                
+            case let .setLoginStatus(isLoggedIn):
+                state.isLoggedIn = isLoggedIn
+                return .none
+
+            case let .setLoadingStatus(isLoading):
+                state.isLoading = isLoading
+                return .none
+                
+            default:
+                return .none
+            }
+        }
+    }
+
+    func isValidAccessToken(_ token: String) -> Bool {
+        let components = token.components(separatedBy: ".")
+        guard components.count == 3 else {
+            return false
+        }
+        
+        do {
+            let base64Decoded = components[1]
+                .replacingOccurrences(of: "-", with: "+")
+                .replacingOccurrences(of: "_", with: "/")
+            
+            let paddedBase64 = base64Decoded.padding(toLength: ((base64Decoded.count + 3) / 4) * 4, withPad: "=", startingAt: 0)
+            
+            guard let jsonData = Data(base64Encoded: paddedBase64),
+                  let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+                  let expiration = json["exp"] as? TimeInterval
+            else {
+                return false
+            }
+            
+            return Date().timeIntervalSince1970 < expiration
+        } catch {
+            return false
+        }
+    }
+}
+
+// 스와이프 제스쳐
+extension UINavigationController: @retroactive ObservableObject, @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
