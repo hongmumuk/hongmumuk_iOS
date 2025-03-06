984f0202a1be54f3a012f25196722093c39b7a55
lyoodong <dwryu3079@naver.com>
2025-03-06 21:17:52 +0900

    ✨ Bundel+ 추가
    - 버전 정보 파싱

--- hongmumuk_SwiftUI/Extension/Bundle+.swift
+++ hongmumuk_SwiftUI/Extension/Bundle+.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension Bundle {
+    var fullVersion: String {
+        guard let version = infoDictionary?["CFBundleShortVersionString"] as? String else {
+            return "0.0.0"
+        }
+        let versionComponents = version.split(separator: ".")
+        let major = versionComponents.count > 0 ? versionComponents[0] : "0"
+        let minor = versionComponents.count > 1 ? versionComponents[1] : "0"
+        let patch = versionComponents.count > 2 ? versionComponents[2] : "0"
+        return "\(major).\(minor).\(patch)"
+    }
+}
