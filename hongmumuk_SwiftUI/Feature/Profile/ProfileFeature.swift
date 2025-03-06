6eacc894d2ac72f4b5cc91ea95823bf0a6727623
lyoodong <dwryu3079@naver.com>
2025-03-06 21:19:02 +0900

    ✨ action/state 추가

--- hongmumuk_SwiftUI/Feature/Profile/ProfileFeature.swift
+++ hongmumuk_SwiftUI/Feature/Profile/ProfileFeature.swift
@@ -12,12 +12,15 @@ struct ProfileFeature: Reducer {
     struct State: Equatable {
         var isUser: Bool = false
         var token: String = ""
+        var currentVersion = ""
     }
     
     enum Action: Equatable {
         case onAppear
         case onDismiss
         case checkUser(String?)
+        case loginButtonTapped
+        case setButtonTapped(ProfileSet)
     }
     
     @Dependency(\.keychainClient) var keychainClient
@@ -26,6 +29,8 @@ struct ProfileFeature: Reducer {
         Reduce { state, action in
             switch action {
             case .onAppear:
+                state.currentVersion = "v" + Bundle.main.fullVersion
+                
                 return .run { send in
                     let token = await keychainClient.getString(.accessToken)
                     await send(.checkUser(token))
@@ -38,6 +43,23 @@ struct ProfileFeature: Reducer {
                 if let token {
                     state.isUser = true
                     state.token = token
+                }
+                
+                return .none
+                
+            case .loginButtonTapped:
+                return .none
+                
+            case let .setButtonTapped(type):
+                switch type {
+                case .info:
+                    print("info")
+                case .service:
+                    print("service")
+                case .privacy:
+                    print("privacy")
+                case .version:
+                    print("version")
                 }
                 
                 return .none
