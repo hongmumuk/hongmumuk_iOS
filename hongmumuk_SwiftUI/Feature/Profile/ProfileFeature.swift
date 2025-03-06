6f654765467e8a7e7295380e94a2eea79984e31f
lyoodong <dwryu3079@naver.com>
2025-03-06 20:39:57 +0900

    ✨ store 초기 상태 추가

--- hongmumuk_SwiftUI/Feature/Profile/ProfileFeature.swift
+++ hongmumuk_SwiftUI/Feature/Profile/ProfileFeature.swift
@@ -5,4 +5,43 @@
 //  Created by Dongwan Ryoo on 3/6/25.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct ProfileFeature: Reducer {
+    struct State: Equatable {
+        var isUser: Bool = false
+        var token: String = ""
+    }
+    
+    enum Action: Equatable {
+        case onAppear
+        case onDismiss
+        case checkUser(String?)
+    }
+    
+    @Dependency(\.keychainClient) var keychainClient
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .run { send in
+                    let token = await keychainClient.getString(.accessToken)
+                    await send(.checkUser(token))
+                }
+                
+            case .onDismiss:
+                return .none
+                
+            case let .checkUser(token):
+                if let token {
+                    state.isUser = true
+                    state.token = token
+                }
+                
+                return .none
+            }
+        }
+    }
+}
