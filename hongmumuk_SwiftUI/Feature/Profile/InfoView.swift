acd7e2d77cbaf30f3870de09edd36f4aa20d9dc3
lyoodong <dwryu3079@naver.com>
2025-03-07 22:03:12 +0900

    💄 emailTextField 추가

--- hongmumuk_SwiftUI/Feature/Profile/InfoView.swift
+++ hongmumuk_SwiftUI/Feature/Profile/InfoView.swift
@@ -17,6 +17,8 @@ struct InfoView: View {
         VStack(alignment: .leading, spacing: 0) {
             nickNameTitle
             nickNameTextFieldStack
+            emailTitle
+            emailTextFieldStack
             Spacer()
         }
     }
@@ -62,5 +64,51 @@ struct InfoView: View {
         }
         .frame(width: 104, height: 48)
         .cornerRadius(12)
+    }
+    
+    private var emailTitle: some View {
+        Text("이메일")
+            .fontStyle(Fonts.heading2Bold)
+            .foregroundStyle(Colors.GrayScale.grayscale95)
+            .padding(.leading, 24)
+            .padding(.top, 24)
+    }
+    
+    private var emailTextFieldStack: some View {
+        VStack(spacing: 8) {
+            emailTextField
+            verificationTitle
+        }
+        .padding(.horizontal, 24)
+        .padding(.top, 8)
+    }
+    
+    private var emailTextField: some View {
+        CommonTextFieldView(
+            isFocused: $isNickNameFocused,
+            text: viewStore.profile.email,
+            state: .disabled,
+            message: nil,
+            placeholder: viewStore.profile.nickName,
+            isSecure: false,
+            onTextChanged: { _ in },
+            onFocusedChanged: { _ in },
+            onSubmit: {},
+            onClear: {}
+        )
+    }
+    
+    private var verificationTitle: some View {
+        HStack(spacing: 4) {
+            Spacer()
+            
+            Text("\(viewStore.todayString) 인증 완료")
+                .fontStyle(Fonts.caption1Medium)
+                .foregroundStyle(Colors.GrayScale.grayscal45)
+            
+            Image("verificationIcon")
+                .resizable()
+                .frame(width: 12, height: 12)
+        }
     }
 }
