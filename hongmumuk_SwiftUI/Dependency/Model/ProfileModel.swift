e666f3b964e6eff57ba6fac19bdaa9767339f302
lyoodong <dwryu3079@naver.com>
2025-03-07 20:53:20 +0900

    ✨ProfileClient 추가

---
+++ hongmumuk_SwiftUI/Dependency/Model/ProfileModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 3/7/25.
+//
+
+import Foundation
+
+struct ProfileModel: Codable {
+    let nickName: String
+    let email: String
+}
