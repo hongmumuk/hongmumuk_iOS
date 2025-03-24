e49f05bb488bb0cc160a1ba4f67fef7c3c9af1f7
30 <1224tpdud@gmail.com>
2025-03-24 21:48:29 +0900

    ✨ 토큰 만료 로그아웃 이동 Notification 추가

---
+++ hongmumuk_SwiftUI/Utility/Notification.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Park Seyoung on 3/24/25.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let shouldLogout = Notification.Name("shouldLogout")
+}
