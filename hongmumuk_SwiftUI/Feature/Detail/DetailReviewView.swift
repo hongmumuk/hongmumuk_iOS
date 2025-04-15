c99107a036808789b539e1299a9bb668adc3dc95
lyoodong <dwryu3079@naver.com>
2025-04-15 16:06:21 +0900

    ♻️ 추가 수정

--- hongmumuk_SwiftUI/Feature/Detail/DetailReviewView.swift
+++ hongmumuk_SwiftUI/Feature/Detail/DetailReviewView.swift
@@ -43,7 +43,7 @@ struct DetailReviewView: View {
             Image("naverIcon")
                 .frame(width: 16, height: 16)
             
-            Text("네이버 블로그 검색 기반 결과입니다")
+            Text("source_from_naverBlog".localized())
                 .fontStyle(Fonts.body1SemiBold)
                 .foregroundColor(Colors.GrayScale.grayscal45)
         }
