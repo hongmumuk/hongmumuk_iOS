8692902b6ea21edcdd03259505b0c2376ada05f7
30 <127213160+30isdead@users.noreply.github.com>
2025-06-05 14:26:33 +0900

    [feat] 앱스플라이어 딥링크 적용 (#92)
    * ✨Add appsflyer
    
    * ♻️ Remove ATT Popup

--- hongmumuk_SwiftUI/hongmumuk_SwiftUIApp.swift
+++ hongmumuk_SwiftUI/hongmumuk_SwiftUIApp.swift
@@ -5,11 +5,18 @@
 //  Created by Dongwan Ryoo on 1/19/25.
 //
 
+import AppTrackingTransparency
 import ComposableArchitecture
 import SwiftUI
 
 @main
 struct Hongmumuk_SwiftUIApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        requestTrackingAuthorizationIfNeeded()
+    }
+    
     var body: some Scene {
         WindowGroup {
             RootView(
@@ -20,6 +27,16 @@ struct Hongmumuk_SwiftUIApp: App {
                     }
                 )
             )
+        }
+    }
+
+    private func requestTrackingAuthorizationIfNeeded() {
+        if #available(iOS 14, *) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                ATTrackingManager.requestTrackingAuthorization { status in
+                    print("ATT Authorization status: \(status.rawValue)")
+                }
+            }
         }
     }
 }
