c2a4c26ed3f18ea9d8ec7c24aec72669416638a7
lyoodong <dwryu3079@naver.com>
2025-07-28 15:38:14 +0900

    ✨ 리뷰 작성 API 추가

---
+++ hongmumuk_SwiftUI/Dependency/Model/WriteReivewModel.swift
@@ -0,0 +1,14 @@
+//
+//  WriteReivewModel.swift
+//  hongmumuk_SwiftUI
+//
+//  Created by Dongwan Ryoo on 7/28/25.
+//
+
+import Foundation
+
+struct WriteReviewModel: Codable {
+    var rid: Int
+    var star: Int
+    var content: String
+}
