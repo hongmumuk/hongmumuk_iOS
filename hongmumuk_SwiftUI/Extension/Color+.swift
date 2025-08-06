c77556b0dadfa7dfe7d2a30bd7b3e18c266d050a
Seyoung Park <seyoungpark@Seyoungs-MacBook-Air.local>
2025-08-06 18:51:29 +0900

    ♻️ 컬러 익스텐션 확장

--- hongmumuk_SwiftUI/Extension/Color+.swift
+++ hongmumuk_SwiftUI/Extension/Color+.swift
@@ -9,19 +9,29 @@ import SwiftUI
 
 extension Color {
     init(hex: String) {
-        let scanner = Scanner(string: hex)
-        
-        if hex.hasPrefix("#") {
-            scanner.currentIndex = hex.index(after: hex.startIndex)
-        }
-        
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexString = hexString.replacingOccurrences(of: "#", with: "")
+
         var rgb: UInt64 = 0
-        scanner.scanHexInt64(&rgb)
-        
-        let r = Double((rgb >> 16) & 0xFF) / 255.0
-        let g = Double((rgb >> 8) & 0xFF) / 255.0
-        let b = Double(rgb & 0xFF) / 255.0
-        
-        self.init(red: r, green: g, blue: b)
+        Scanner(string: hexString).scanHexInt64(&rgb)
+
+        let r, g, b, a: Double
+
+        switch hexString.count {
+        case 6:
+            r = Double((rgb >> 16) & 0xFF) / 255.0
+            g = Double((rgb >> 8) & 0xFF) / 255.0
+            b = Double(rgb & 0xFF) / 255.0
+            a = 1.0
+        case 8:
+            r = Double((rgb >> 24) & 0xFF) / 255.0
+            g = Double((rgb >> 16) & 0xFF) / 255.0
+            b = Double((rgb >> 8) & 0xFF) / 255.0
+            a = Double(rgb & 0xFF) / 255.0
+        default:
+            r = 0; g = 0; b = 0; a = 1.0
+        }
+
+        self.init(red: r, green: g, blue: b, opacity: a)
     }
 }
