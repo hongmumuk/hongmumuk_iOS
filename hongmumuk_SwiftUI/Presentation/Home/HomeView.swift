a9f1f6971e4535eb9951c4c3acec01533a25cbc8
lyoodong <dwryu3079@naver.com>
2026-01-04 15:12:09 +0900

    [add] homeView 생성

--- hongmumuk_SwiftUI/Presentation/Home/HomeView.swift
+++ hongmumuk_SwiftUI/Presentation/Home/HomeView.swift
@@ -1,1 +1,34 @@
+import SwiftUI
 
+struct HomeView: View {
+    @State var showDetail = false
+    
+    var body: some View {
+        ScrollView(content: content)
+            .fullScreenCover(isPresented: $showDetail, content: fullScreenContent)
+            .padding(.top)
+    }
+    
+    private func content() -> some View {
+        VStack(spacing: 0) {
+            HMLargeTitle(title: "오늘의 추천 맛집")
+            HMLagePhotoList()
+                .onTapGesture { showDetail = true }
+            HMLargeTitle(title: "에디터 추천 5선")
+            HMMediumPhotoList()
+                .onTapGesture { showDetail = true }
+            HMLargeTitle(title: "새로운 장소가 궁금하다면?")
+            HMSmallPhotoList(items: tagSmallCards)
+                .onTapGesture { showDetail = true }
+            HMLargeTitle(title: "카테고리별로 볼래요")
+            HMFilter(isImage: true)
+            HMSmallPhotoList(items: categorySmallCards)
+                .onTapGesture { showDetail = true }
+            Spacer()
+        }
+    }
+    
+    private func fullScreenContent() -> some View {
+        return DetailView()
+    }
+}
