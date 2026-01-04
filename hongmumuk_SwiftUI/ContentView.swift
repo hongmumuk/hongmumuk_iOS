894ac9ad8098001b15c67e586dbe0b66e4aef1c9
lyoodong <dwryu3079@naver.com>
2026-01-04 15:12:09 +0900

    [add] homeView 생성

--- hongmumuk_SwiftUI/ContentView.swift
+++ hongmumuk_SwiftUI/ContentView.swift
@@ -12,33 +12,12 @@ struct ContentView: View {
     
     var body: some View {
         TabView {
-            ScrollView {
-                VStack(spacing: 0) {
-                    HMLargeTitle(title: "오늘의 추천 맛집")
-                    HMLagePhotoList()
-                        .onTapGesture { showDetail = true }
-                    HMLargeTitle(title: "에디터 추천 5선")
-                    HMMediumPhotoList()
-                        .onTapGesture { showDetail = true }
-                    HMLargeTitle(title: "새로운 장소가 궁금하다면?")
-                    HMSmallPhotoList(items: tagSmallCards)
-                        .onTapGesture { showDetail = true }
-                    HMLargeTitle(title: "카테고리별로 볼래요")
-                    HMFilter(isImage: true)
-                    HMSmallPhotoList(items: categorySmallCards)
-                        .onTapGesture { showDetail = true }
-                    Spacer()
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
                 }
-            }
-            .fullScreenCover(isPresented: $showDetail) {
-                DetailView()
-            }
-            .padding(.top)
-            .tabItem {
-                Image(systemName: "house.fill")
-                Text("홈")
-            }
-
+            
             ScrollView {
                 VStack(spacing: 0) {
                     HMFilter(isImage: false)
