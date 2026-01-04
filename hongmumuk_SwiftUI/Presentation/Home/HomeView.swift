8cecec8f0ee2a33afc2b5050ee618b2ecf345f04
lyoodong <dwryu3079@naver.com>
2026-01-04 15:53:34 +0900

    [add] 데이터 전체 연결

--- hongmumuk_SwiftUI/Presentation/Home/HomeView.swift
+++ hongmumuk_SwiftUI/Presentation/Home/HomeView.swift
@@ -2,29 +2,56 @@ import SwiftUI
 
 struct HomeView: View {
     @State var showDetail = false
+    var homeViewModel: HomeViewModel = .init()
     
     var body: some View {
         ScrollView(content: content)
             .fullScreenCover(isPresented: $showDetail, content: fullScreenContent)
             .padding(.top)
+            .task {
+                await homeViewModel.getSections()
+            }
     }
     
+    @ViewBuilder
     private func content() -> some View {
-        VStack(spacing: 0) {
-            HMLargeTitle(title: "오늘의 추천 맛집")
-            HMLagePhotoList()
-                .onTapGesture { showDetail = true }
-            HMLargeTitle(title: "에디터 추천 5선")
-            HMMediumPhotoList()
-                .onTapGesture { showDetail = true }
-            HMLargeTitle(title: "새로운 장소가 궁금하다면?")
-            HMSmallPhotoList(items: tagSmallCards)
-                .onTapGesture { showDetail = true }
-            HMLargeTitle(title: "카테고리별로 볼래요")
-            HMFilter(isImage: true)
-            HMSmallPhotoList(items: categorySmallCards)
-                .onTapGesture { showDetail = true }
-            Spacer()
+        if let sections = homeViewModel.items?.sections {
+            VStack(spacing: 0) {
+                ForEach(sections) { section in
+                    print("title", section.props.title)
+                    
+                    return VStack(spacing: 0) {
+                        if let title = section.props.title {
+                            HMLargeTitle(title: title)
+                        }
+                        
+                        switch section.type {
+                        case .cards:
+                            switch section.props.cardStyle {
+                            case .large:
+                                HMLagePhotoList()
+                                    .onTapGesture { showDetail = true }
+                            case .medium:
+                                HMMediumPhotoList()
+                                    .onTapGesture { showDetail = true }
+                            case .small:
+                                HMSmallPhotoList(items: tagSmallCards)
+                                    .onTapGesture { showDetail = true }
+                            case .none:
+                                Spacer()
+                            }
+                        case .categoryFilterList:
+                            VStack(spacing: 0) {
+                                HMFilter(isImage: true)
+                                HMSmallPhotoList(items: categorySmallCards)
+                                    .onTapGesture { showDetail = true }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            Text("데이터가 없습니다.")
         }
     }
     
@@ -32,3 +59,18 @@ struct HomeView: View {
         return DetailView()
     }
 }
+
+//            HMLargeTitle(title: "오늘의 추천 맛집")
+//            HMLagePhotoList()
+//                .onTapGesture { showDetail = true }
+//            HMLargeTitle(title: "에디터 추천 5선")
+//            HMMediumPhotoList()
+//                .onTapGesture { showDetail = true }
+//            HMLargeTitle(title: "새로운 장소가 궁금하다면?")
+//            HMSmallPhotoList(items: tagSmallCards)
+//                .onTapGesture { showDetail = true }
+//            HMLargeTitle(title: "카테고리별로 볼래요")
+//            HMFilter(isImage: true)
+//            HMSmallPhotoList(items: categorySmallCards)
+//                .onTapGesture { showDetail = true }
+//            Spacer()
