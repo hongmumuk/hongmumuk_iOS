686b05bbc1ade5ae5a8204d2e969dc8c1d66093b
lyoodong <dwryu3079@naver.com>
2026-01-07 13:37:54 +0900

    [add] UI 연결

--- hongmumuk_SwiftUI/Presentation/Partner/PartnerlView.swift
+++ hongmumuk_SwiftUI/Presentation/Partner/PartnerlView.swift
@@ -1,21 +1,48 @@
 import SwiftUI
 
 struct PartnerlView: View {
+    @State var showDetail = false
     var partnerViewModel: PartnerViewModel = .init()
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 0) {
-                HMFilter(isImage: false)
-                HMLargeTitle(title: "카페")
-//                    HMSmallPhotoList(items: cafeBenefitCards)
-                HMLargeTitle(title: "음식점")
-//                    HMSmallPhotoList(items: restaurantBenefitCards)
-                HMLargeTitle(title: "생활/문화")
-//                    HMSmallPhotoList(items: lifeCultureBenefitCards)
-                Spacer()
+        ScrollView(content: content)
+            .fullScreenCover(isPresented: $showDetail, content: fullScreenContent)
+            .padding(.top)
+            .task {
+                await partnerViewModel.getSections()
             }
+    }
+    
+    @ViewBuilder
+    private func content() -> some View {
+        if !partnerViewModel.sections.isEmpty {
+            VStack(spacing: 0) {
+                ForEach(partnerViewModel.sections, id: \.id) { section in
+                    switch section.type {
+                    case .title:
+                        if let item = section as? HMLTitle {
+                            HMLargeTitle(title: item.title)
+                        }
+                        
+                    case .partnerSmallPhoto:
+                        if let item = section as? HMPartnerSmallPhotos {
+                            VStack {
+                                HMFilter(isImage: false)
+                                HMSmallPhotoList(cards: item.items)
+                            }
+                        }
+                        
+                    default:
+                        EmptyView()
+                    }
+                }
+            }
+        } else {
+            Text("데이터가 없습니다.")
         }
-        .padding(.top)
+    }
+    
+    private func fullScreenContent() -> some View {
+        return DetailView()
     }
 }
